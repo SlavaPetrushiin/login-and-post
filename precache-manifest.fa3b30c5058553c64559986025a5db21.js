e38d9d676c1f407d85b779072bec61459d9d48c4
Slava <slava91petrushin@yandex.ru>
2020-04-13 19:53:59 +0300

    Updates

---
+++ precache-manifest.fa3b30c5058553c64559986025a5db21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b744969f81d071c78e65393306f68c72",
+    "url": "/login-and-post/index.html"
+  },
+  {
+    "revision": "d985d5c53bd354acd786",
+    "url": "/login-and-post/static/css/main.857e7e9a.chunk.css"
+  },
+  {
+    "revision": "6246457fb6a35dab57ee",
+    "url": "/login-and-post/static/js/2.64683763.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/login-and-post/static/js/2.64683763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d985d5c53bd354acd786",
+    "url": "/login-and-post/static/js/main.1cfbe2a2.chunk.js"
+  },
+  {
+    "revision": "49db6b1191876f5f20a1",
+    "url": "/login-and-post/static/js/runtime-main.cd3b9236.js"
+  }
+]);
